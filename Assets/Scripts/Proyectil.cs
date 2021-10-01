d7f1bfce1f4846bd901a9f9f9e3848e0b5746617
RaysusGames <86489539+RaysusGames@users.noreply.github.com>
2021-10-01 12:46:23 -0300

    clase 1 complete

---
+++ Assets/Scripts/Proyectil.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Proyectil : MonoBehaviour
+{
+    public float initialVelocity = 10;
+    
+public void ShootTo(Vector3 Direction)
+    {
+        GetComponent<Rigidbody>().AddForce(Direction * initialVelocity,ForceMode.Impulse);
+    }
+
+
+    public void ShootWitchForce(Vector3 Direction , float force)
+    {
+        GetComponent<Rigidbody>().AddForce(Direction*force,ForceMode.Impulse);
+    }
+
+    IEnumerator RestoreCollision()
+    {
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<Collider>().isTrigger = false;
+    }
+}
---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{ 
+    public GameObject projectil;
+    public Transform aimDir;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void AimShooting()
+    {
+        float v = Input.GetAxis("Vertical");
+        aimDir.Rotate(v*-1,0,0,Space.Self);
+    }
+     
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate()
+        }
+    }
+    
+}
